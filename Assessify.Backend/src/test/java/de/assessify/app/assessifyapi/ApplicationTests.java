657c51f868ce5087a6b1518ef2979a00db3fbe37
Sebastian <145010047+sebiwsk@users.noreply.github.com>
2025-09-09 07:55:18 +0200

    feat: initial commit

---
+++ Assessify.Backend/src/test/java/de/assessify/app/assessifyapi/ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.assessify.app.assessifyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
