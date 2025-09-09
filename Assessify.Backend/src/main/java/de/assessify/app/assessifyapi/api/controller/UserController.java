631653bdd9276501127e7c4d1b3cad388691f0fe
Sebastian <145010047+sebiwsk@users.noreply.github.com>
2025-09-09 10:50:51 +0200

    feat: GetAllUser Controller

--- Assessify.Backend/src/main/java/de/assessify/app/assessifyapi/api/controller/UserController.java
+++ Assessify.Backend/src/main/java/de/assessify/app/assessifyapi/api/controller/UserController.java
@@ -1,21 +1,32 @@
 package de.assessify.app.assessifyapi.api.controller;
 
+import de.assessify.app.assessifyapi.api.UserRepository.UserRepository;
 import de.assessify.app.assessifyapi.api.model.User;
 import de.assessify.app.assessifyapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/api/users")
 public class UserController {
+    private UserRepository userRepository;
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Autowired
     public UserService userService;
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
     }
     @GetMapping
-    public User getUser(@RequestParam Integer id) {
-        return userService.getUser(id);
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
     }
 }
