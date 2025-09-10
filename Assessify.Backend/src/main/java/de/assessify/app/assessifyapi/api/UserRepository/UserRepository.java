cfd2d61f59534e6f7671669304015a608bc1b6f3
georgdatagraphis <156414420+georgdatagraphis@users.noreply.github.com>
2025-09-10 10:59:16 +0200

    [FEATURE]postgres DB created + RESTAPI works for POST

--- Assessify.Backend/src/main/java/de/assessify/app/assessifyapi/api/UserRepository/UserRepository.java
+++ Assessify.Backend/src/main/java/de/assessify/app/assessifyapi/api/UserRepository/UserRepository.java
@@ -4,6 +4,7 @@ package de.assessify.app.assessifyapi.api.UserRepository;
 import de.assessify.app.assessifyapi.api.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<User, String>{
+public interface UserRepository extends JpaRepository<User, UUID>{
 }
