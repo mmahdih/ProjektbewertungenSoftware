7fae51b94af3bc4719cde1fd233e97ee9dbf131b
laurinelias <laurinschubert02@gmail.com>
2025-09-10 10:19:38 +0200

    feat: add all Teammates

--- frontend/src/app/selbstfremdbewertung/selbstfremd.ts
+++ frontend/src/app/selbstfremdbewertung/selbstfremd.ts
@@ -19,6 +19,10 @@ import { CommonModule } from '@angular/common';
 export class selbstfremd {
   rating = 0;
   rating2 = 0;
+  rating3 = 0;
+  rating4 = 0;
+  rating5 = 0;
+  grades: number[] = [];
 
   setRating(value: number) {
     this.rating = value;
@@ -26,9 +30,28 @@ export class selbstfremd {
   setRating2(value: number) {
     this.rating2 = value;
   }
+  setRating3(value: number) {
+    this.rating3 = value;
+  }
+  setRating4(value: number) {
+    this.rating4 = value;
+  }
+  setRating5(value: number) {
+    this.rating5 = value;
+  }
 
   submitRating() {
     console.log('Bewertung gespeichert:', this.rating, this.rating2);
-    // hier kannst du die Bewertung ans Backend schicken
+    this.grades.push(this.rating);
+    this.grades.push(this.rating2);
+    this.grades.push(this.rating3);
+    this.grades.push(this.rating4);
+    this.grades.push(this.rating5);
+    console.log(this.grades);
+    this.rating = 0;
+    this.rating2 = 0;
+    this.rating3 = 0;
+    this.rating4 = 0;
+    this.rating5 = 0;
   }
 }
