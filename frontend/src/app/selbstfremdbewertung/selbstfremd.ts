b9557d366b110256d203ac5cab51d69d3f18ffad
laurinelias <laurinschubert02@gmail.com>
2025-09-10 11:40:27 +0200

    chore: add dynamic activeX classes for colored button states and list for questions

--- frontend/src/app/selbstfremdbewertung/selbstfremd.ts
+++ frontend/src/app/selbstfremdbewertung/selbstfremd.ts
@@ -17,12 +17,16 @@ import { CommonModule } from '@angular/common';
   CommonModule]
 })
 export class selbstfremd {
+
+    test: string[] = ['Frage1', 'Frage2', 'Frage3', 'Frage4', 'Frage5'];
+    frage =  0; 
   rating = 0;
   rating2 = 0;
   rating3 = 0;
   rating4 = 0;
   rating5 = 0;
   grades: number[] = [];
+  grades2: number[] = [];
 
   setRating(value: number) {
     this.rating = value;
@@ -54,4 +58,29 @@ export class selbstfremd {
     this.rating4 = 0;
     this.rating5 = 0;
   }
+
+ members = [
+  { id: 0, name: 'Teammitglied 1' },
+  { id: 1, name: 'Teammitglied 2' },
+  { id: 2, name: 'Teammitglied 3' },
+  { id: 3, name: 'Teammitglied 4' },
+  { id: 4, name: 'Teammitglied 5' }
+];
+ratings: number[] = this.members.map(_ => 0);
+
+setRating99(memberId: number, value: number) {
+  this.ratings[memberId] = value;
+  let technotest = 'active4';
+}
+
+  setGrades(memberIndex: number, value: number) {
+    console.log(`Set rating for member ${memberIndex+1} to ${value}`);
+    this.ratings[memberIndex] = value;
+  }
+submitRating2() {
+    console.log('Bewertung gespeichert:', this.ratings); // Kopiere die aktuellen Bewertungen in grades2
+    this.ratings = [0, 0, 0, 0, 0]; // Setze alle Bewertungen zurück
+    this.frage++;
+  }
+
 }
