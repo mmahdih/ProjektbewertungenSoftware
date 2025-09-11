f8ad2e4110e7b3c4ffe097a634196e30bdc1911e
georgdatagraphis <156414420+georgdatagraphis@users.noreply.github.com>
2025-09-11 10:27:31 +0200

    [FEATURE]Loginfunktion in AuthService ausgelagert

--- Assessify.Backend/src/main/java/de/assessify/app/assessifyapi/api/controller/AuthController.java
+++ Assessify.Backend/src/main/java/de/assessify/app/assessifyapi/api/controller/AuthController.java
@@ -3,6 +3,7 @@ package de.assessify.app.assessifyapi.api.controller;
 import de.assessify.app.assessifyapi.api.UserRepository.UserRepository;
 import de.assessify.app.assessifyapi.api.model.LoginDto;
 import de.assessify.app.assessifyapi.api.model.User;
+import de.assessify.app.assessifyapi.api.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,28 +15,18 @@ import java.util.Optional;
 @RequestMapping("/api/auth")
 public class AuthController {
 
-    private final UserRepository userRepository;
-    private final BCryptPasswordEncoder passwordEncoder;
+    private final AuthService authService;
 
     @Autowired
-    public AuthController(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
+    public AuthController(AuthService authService) {
+        this.authService = authService;
     }
 
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody LoginDto loginDto) {
-        // Findet den Benutzer anhand der E-Mail
-        Optional<User> userOptional = userRepository.findByEmail(loginDto.getEmail());
-
-        if (userOptional.isPresent()) {
-            User user = userOptional.get();
-            // Überprüft, ob das eingegebene Passwort mit dem gehashten Passwort in der Datenbank übereinstimmt
-            if (passwordEncoder.matches(loginDto.getPassword(), user.getPassword())) {
-                return new ResponseEntity<>("Login successful!", HttpStatus.OK);
-            }
+        if (authService.authenticateUser(loginDto.getEmail(), loginDto.getPassword())) {
+            return new ResponseEntity<>("Login successful!", HttpStatus.OK);
         }
-        // Fehler, wenn der Benutzer nicht existiert oder das Passwort falsch ist
         return new ResponseEntity<>("Invalid email or password", HttpStatus.UNAUTHORIZED);
     }
 }
