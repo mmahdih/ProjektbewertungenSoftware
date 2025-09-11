211d9f4582890ba09aa12716d704f4ea13d3da3d
georgdatagraphis <156414420+georgdatagraphis@users.noreply.github.com>
2025-09-11 10:27:31 +0200

    [FEATURE]Loginfunktion in AuthService ausgelagert

--- Assessify.Backend/src/main/java/de/assessify/app/assessifyapi/api/service/AuthService.java
+++ Assessify.Backend/src/main/java/de/assessify/app/assessifyapi/api/service/AuthService.java
@@ -1,4 +1,32 @@
 package de.assessify.app.assessifyapi.api.service;
 
+import de.assessify.app.assessifyapi.api.UserRepository.UserRepository;
+import de.assessify.app.assessifyapi.api.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class AuthService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    public AuthService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public boolean authenticateUser(String email, String password) {
+        Optional<User> userOptional = userRepository.findByEmail(email);
+
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            return passwordEncoder.matches(password, user.getPassword());
+        }
+        return false;
+    }
 }
