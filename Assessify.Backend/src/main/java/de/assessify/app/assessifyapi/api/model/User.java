09ab8f2ee75ddf961ac6cb29a5827b749879dcdd
Sebastian <145010047+sebiwsk@users.noreply.github.com>
2025-10-04 17:19:30 +0200

    refactor: remove unused Stuff

--- Assessify.Backend/src/main/java/de/assessify/app/assessifyapi/api/model/User.java
+++ Assessify.Backend/src/main/java/de/assessify/app/assessifyapi/api/model/User.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-
 @Entity
 @Data
 @Table(name = "users")
@@ -57,61 +56,6 @@ public class User {
             inverseJoinColumns = @JoinColumn(name = "class_id")
     )
     private List<SchoolClass> schoolClasses = new ArrayList<>();
-
-    public User() {
-    }
-
-    public User(String firstName, String lastName, String email) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-    }
-
-    @PrePersist
-    protected void onCreate() {
-        this.createdAt = LocalDateTime.now();
-    }
-
-    // Getters und Setters
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
 
     public String getPassword() { return password; }
 
