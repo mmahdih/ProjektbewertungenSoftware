217f26c734919ce1d351c29c1170970380ad0daa
Laurin Schubert <Laurin.Schubert@sva.de>
2025-11-20 12:30:21 +0100

    chore: classes/group

--- frontend/src/app/Features/Student/my-classes/my-classes.ts
+++ frontend/src/app/Features/Student/my-classes/my-classes.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
+import { Sidebar } from "../../../layout/sidebar/sidebar";
+
 
 @Component({
   selector: 'app-my-classes',
-  imports: [MatIconModule],
+  imports: [Sidebar],
   templateUrl: './my-classes.html',
   styleUrl: './my-classes.css'
 })
