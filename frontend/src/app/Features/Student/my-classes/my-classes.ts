dfbec2393621697bd554aa8e7f2c1c15886234fe
Laurin Schubert <Laurin.Schubert@sva.de>
2025-11-20 12:30:21 +0100

    chore: classes/group

--- frontend/src/app/Features/Student/my-classes/my-classes.ts
+++ frontend/src/app/Features/Student/my-classes/my-classes.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Sidebar } from "../../../layout/sidebar/sidebar";
+import { DashboardNavbar } from "../../../Shared/Components/dashboard-navbar/dashboard-navbar";
 
 @Component({
   selector: 'app-my-classes',
-  imports: [],
+  imports: [Sidebar, DashboardNavbar],
   templateUrl: './my-classes.html',
   styleUrl: './my-classes.css'
 })
