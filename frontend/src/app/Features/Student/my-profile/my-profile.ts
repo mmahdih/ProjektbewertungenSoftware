44ce0fadda779107080ce165a4dccc7a2b29a9d0
Laurin Schubert <Laurin.Schubert@sva.de>
2025-11-20 12:29:49 +0100

    feat: add base Card for Grades overview

--- frontend/src/app/Features/Student/my-profile/my-profile.ts
+++ frontend/src/app/Features/Student/my-profile/my-profile.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { Sidebar } from "../../../layout/sidebar/sidebar";
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from "@angular/material/icon";
+
 
 @Component({
   selector: 'app-my-profile',
-  imports: [MatIconModule],
+  imports: [ MatCardModule, MatListModule, MatIconModule],
   templateUrl: './my-profile.html',
   styleUrl: './my-profile.css'
 })
