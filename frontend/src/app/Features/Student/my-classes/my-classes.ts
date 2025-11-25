9dc3d975f62ad2a72da4f7e3f6141f491ad75b98
Laurin Schubert <Laurin.Schubert@sva.de>
2025-11-25 09:15:35 +0100

    feat: add table basic table

--- frontend/src/app/Features/Student/my-classes/my-classes.ts
+++ frontend/src/app/Features/Student/my-classes/my-classes.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Sidebar } from "../../../layout/sidebar/sidebar";
 import { DashboardNavbar } from "../../../Shared/Components/dashboard-navbar/dashboard-navbar";
+import { MatIcon } from "@angular/material/icon";
 
 @Component({
   selector: 'app-my-classes',
-  imports: [Sidebar, DashboardNavbar],
+  imports: [Sidebar, DashboardNavbar, MatIcon],
   templateUrl: './my-classes.html',
   styleUrl: './my-classes.css'
 })
