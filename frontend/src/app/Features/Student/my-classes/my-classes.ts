1577d4c70223a6ab993d1a0a669f83e39fa3e11c
Laurin Schubert <Laurin.Schubert@sva.de>
2025-11-26 13:03:30 +0100

    feat: add groupmember with a dynamic list

--- frontend/src/app/Features/Student/my-classes/my-classes.ts
+++ frontend/src/app/Features/Student/my-classes/my-classes.ts
@@ -3,6 +3,12 @@ import { Sidebar } from "../../../layout/sidebar/sidebar";
 import { DashboardNavbar } from "../../../Shared/Components/dashboard-navbar/dashboard-navbar";
 import { MatIcon } from "@angular/material/icon";
 
+export interface Student {
+id: number;
+firstName: string;
+lastName: string;
+email: string;
+} 
 @Component({
   selector: 'app-my-classes',
   imports: [Sidebar, DashboardNavbar, MatIcon],
@@ -10,5 +16,14 @@ import { MatIcon } from "@angular/material/icon";
   styleUrl: './my-classes.css'
 })
 export class MyClasses {
+
+  students : Student[]=[
+    { id: 0, firstName: 'Max' ,lastName :'Mustermann',email:'test@email.com'},
+    { id: 1, firstName: 'Rainer' ,lastName :'Zufall',email:'test@email.de'},
+    { id: 2, firstName: 'Peter' ,lastName :'Silie',email:'123@email.com'},
+    { id: 3, firstName: 'Tim' ,lastName :'Buktu',email:'email@email.com'},
+    { id: 4, firstName: 'Anna' ,lastName :'Log',email:'e@mail.txt'},
+    { id: 5, firstName: 'Mona' ,lastName :'Lisa',email:'mona@lisa.koop'},
+  ];
 
 }
