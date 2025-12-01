b82727fbf99a3c3b36e58291b9cb50189da0611b
Laurin Schubert <Laurin.Schubert@sva.de>
2025-12-01 11:25:51 +0100

    feat: add dynamic selbstfremd

--- frontend/src/app/Features/Student/my-assessment/my-assessment.ts
+++ frontend/src/app/Features/Student/my-assessment/my-assessment.ts
@@ -6,22 +6,14 @@ import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-my-results',
-   imports: [
-    ReactiveFormsModule,
-    MatCardModule,
-    MatButtonModule,
-    MatIconModule,
-    CommonModule,
-],
+  imports: [ReactiveFormsModule, MatCardModule, MatButtonModule, MatIconModule, CommonModule],
   templateUrl: './my-assessment.html',
-  styleUrl: './my-assessment.css'
+  styleUrl: './my-assessment.css',
 })
 export class MyAssessment {
-  
-
   form: FormGroup;
-  
-  bewertung = new Map<string,number[]>();
+
+  bewertung = new Map<string, number[]>();
   questions: string[] = [
     '1: Wie schätzen sie das Engagment im Projekt ein?',
     '2: Wie zielgerichtet wurde an der Aufgabenstellung gearbeitet?',
@@ -36,61 +28,67 @@ export class MyAssessment {
     '11: Welche Gesamtnote würen Sie der jeweiligen Person für Ihren beitrag zum Gelingen des Projektes geben?'
   ];
 
-  frage =  0;
+  frage = 0;
+  commitedRating = false;
 
- members = [
-  { id: 0, name: 'Teammitglied 1' },
-  { id: 1, name: 'Teammitglied 2' },
-  { id: 2, name: 'Teammitglied 3' },
-  { id: 3, name: 'Teammitglied 4' },
-  { id: 4, name: 'Teammitglied 5' }
-];
+  members = [
+    { id: 0, name: 'Teammitglied 1' },
+    { id: 1, name: 'Teammitglied 2' },
+    { id: 2, name: 'Teammitglied 3' },
+    { id: 3, name: 'Teammitglied 4' },
+    { id: 4, name: 'Teammitglied 5' },
+  ];
 
-ratings: number[] = this.members.map(_ => 0);
+  ratings: number[] = this.members.map((_) => 0);
 
-constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder) {
     // Für jedes Mitglied ein Pflichtfeld (required)
     this.form = this.fb.group({});
-    this.members.forEach(m => {
-      this.form.addControl(
-        `rating_${m.id}`,
-        this.fb.control(null, Validators.required)
-      );
+    this.members.forEach((m) => {
+      this.form.addControl(`rating_${m.id}`, this.fb.control(null, Validators.required));
     });
   }
 
-setRating(memberId: number, value: number) {
-  this.ratings[memberId] = value;
-}
+  setRating(memberId: number, value: number) {
+    this.ratings[memberId] = value;
+  }
 
-deleteAll(){
-  this.bewertung.clear;
-  this.ratings = [];
-  this.frage = 0;
-}
+  deleteAll() {
+    this.bewertung.clear;
+    this.ratings = [];
+    this.frage = 0;
+  }
 
-submitRating() {
+  submitRating() {
     const missing: string[] = [];
 
-    this.members.forEach(m => {
-    const ctrl = this.ratings[m.id];
-    if (ctrl === 0) {
-      missing.push(m.name);
-    }
-  });
+    this.members.forEach((m) => {
+      const ctrl = this.ratings[m.id];
+      if (ctrl === 0) {
+        missing.push(m.name);
+      }
+    });
 
-  if (missing.length > 0) {
-    alert(`❌ Folgende Mitglieder fehlen noch: ${missing.join(', ')}`);
-    this.form.markAllAsTouched();
-  } else { 
-    this.bewertung.set(this.questions[this.frage],this.ratings);
-    this.ratings = [0, 0, 0, 0, 0]; 
-    this.frage++;
-    /**
-     * TODO: bewertung zu JSON umwandeln
-     */
+    if (missing.length > 0) {
+      alert(`❌ Folgende Mitglieder fehlen noch: ${missing.join(', ')}`);
+      this.form.markAllAsTouched();
+    } else {
+      this.bewertung.set(this.questions[this.frage], this.ratings);
+      this.ratings = [0, 0, 0, 0, 0];
+      this.frage++;
+    }
   }
+  sendRating() {
+    const missing: string[] = [];
+    if (missing.length > 0) {
+      alert(`❌ Folgende Mitglieder fehlen noch: ${missing.join(', ')}`);
+      this.form.markAllAsTouched();
+    } else {
+      this.bewertung.set(this.questions[this.frage], this.ratings);
+      this.ratings = [0, 0, 0, 0, 0];
+      this.frage++;
+    }
+    console.log(this.bewertung);
+    this.commitedRating = true;
   }
 }
-
-
