76a4eced77a56e5154bce9f4ea5a9066a4badc25
Mahdi Haidary <mahdi.haidary@sinc.de>
2025-12-02 11:21:54 +0100

    login integration done:

--- Assessify.Backend/src/main/java/de/assessify/app/assessifyapi/api/controller/AuthController.java
+++ Assessify.Backend/src/main/java/de/assessify/app/assessifyapi/api/controller/AuthController.java
@@ -1,5 +1,4 @@
 package de.assessify.app.assessifyapi.api.controller;
-
 import de.assessify.app.assessifyapi.api.dtos.response.LoginResponseDto;
 import de.assessify.app.assessifyapi.api.entity.LoginDto;
 import de.assessify.app.assessifyapi.api.service.AuthService;
@@ -8,9 +7,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200") // ✅ Das hier hinzufügen
 @RequestMapping("/api/auth")
 public class AuthController {
-
     private final AuthService authService;
 
     public AuthController(AuthService authService) {
@@ -20,16 +19,13 @@ public class AuthController {
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginDto loginDto) {
         String token = authService.loginAndGetJwt(loginDto.getUsername(), loginDto.getPassword());
-
         if (token == null) {
             return new ResponseEntity<>("Invalid username or password", HttpStatus.UNAUTHORIZED);
         }
-
         LoginResponseDto response = new LoginResponseDto(
                 token,
                 "Bearer"
         );
-
         return ResponseEntity.ok(response);
     }
 }
