e8d363cbdde9a9dcc4e684681dd587a6d65f748f
Sebastian <145010047+sebiwsk@users.noreply.github.com>
2025-12-02 11:33:46 +0100

    feat: EndPoint get school class by id

--- Assessify.Backend/src/main/java/de/assessify/app/assessifyapi/api/repository/SchoolClassRepository.java
+++ Assessify.Backend/src/main/java/de/assessify/app/assessifyapi/api/repository/SchoolClassRepository.java
@@ -3,6 +3,9 @@ package de.assessify.app.assessifyapi.api.repository;
 import de.assessify.app.assessifyapi.api.entity.SchoolClass;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
-public interface SchoolClassRepository extends JpaRepository<SchoolClass, UUID> {}
+public interface SchoolClassRepository extends JpaRepository<SchoolClass, UUID> {
+    List<SchoolClass> findByUsers_Id(UUID userId);
+}
