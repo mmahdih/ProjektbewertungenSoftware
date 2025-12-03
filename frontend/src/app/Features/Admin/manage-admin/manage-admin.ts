60b0cf328bd145f466cef5c7eed75bb4b69a68ae
Sebastian <145010047+sebiwsk@users.noreply.github.com>
2025-12-03 14:48:56 +0100

    feat: add dynamic components to manage admin view

--- frontend/src/app/Features/Admin/manage-admin/manage-admin.ts
+++ frontend/src/app/Features/Admin/manage-admin/manage-admin.ts
@@ -5,11 +5,18 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { User } from '../../../Interfaces/user.interface';
 import { AuthService } from '../../../core/auth/auth.service'
+import { TableColumn, TableColumnComponent } from '../../../Shared/Components/table-column/table-column';
+import { FormField, FormModalComponent } from '../../../Shared/Components/form-modal/form-modal';
+import { PageHeaderComponents } from '../../../Shared/Components/page-header/page-header';
 
 @Component({
   selector: 'app-manage-admin',
   standalone: true,
-  imports: [CommonModule, MatIconModule, FormsModule],
+  imports: [CommonModule, MatIconModule, FormsModule,
+    PageHeaderComponents,
+    TableColumnComponent,
+    FormModalComponent,
+  ],
   templateUrl: './manage-admin.html'
 })
 export class ManageAdmins implements OnInit{
@@ -17,6 +24,64 @@ export class ManageAdmins implements OnInit{
   loading = true;
 
   showAddModel: boolean = false;
+
+  columns: TableColumn<User>[] = [
+    { key: 'firstName', label: 'First Name' },
+    { key: 'lastName', label: 'Last Name' },
+    { key: 'username', label: 'Username' },
+    { key: 'roleName', label: 'Role' },
+  ];
+
+  fields: FormField[] = [
+    {
+      key: 'firstName',
+      label: 'First Name',
+      type: 'text',
+      required: true,
+      colSpan: 3,
+      placeholder: 'Vorname',
+    },
+    {
+      key: 'lastName',
+      label: 'Last Name',
+      type: 'text',
+      required: true,
+      colSpan: 3,
+      placeholder: 'Nachname',
+    },
+    {
+      key: 'username',
+      label: 'Username',
+      type: 'text',
+      required: true,
+      colSpan: 3,
+      placeholder: 'Benutzername',
+    },
+    {
+      key: 'position',
+      label: 'Position',
+      type: 'text',
+      readonly: true,
+      value: 'ADMIN',
+      colSpan: 3,
+    },
+    {
+      key: 'password',
+      label: 'Password',
+      type: 'password',
+      required: true,
+      colSpan: 3,
+      placeholder: 'Passwort',
+    },
+    {
+      key: 'repeat-password',
+      label: 'Repeat-Password',
+      type: 'password',
+      required: true,
+      colSpan: 3,
+      placeholder: 'Passwort wiederholen',
+    },
+  ];
 
   firstName = '';
   lastName = '';
@@ -35,7 +100,7 @@ export class ManageAdmins implements OnInit{
     this.showAddModel = true;
   }
 
-  closeAddModal(): void {
+  closeAddModel(): void {
     this.showAddModel = false;
   }
 
@@ -53,15 +118,15 @@ export class ManageAdmins implements OnInit{
     });
   }
 
-  saveAdmin() {
+  saveAdmin(formData: any) {
     if (this.password === this.confirmPassword) {
         
     }
     const dto = {
-  firstName: this.firstName,
-  lastName: this.lastName,
-  username: this.username,
-  password: this.password,
+  firstName: formData.firstName,
+  lastName: formData.lastName,
+  username: formData.username,
+  password: formData.password,
   role: this.authService.getRoleId() 
 };
     
@@ -69,7 +134,7 @@ export class ManageAdmins implements OnInit{
     this.AdminService.createAdmin(dto).subscribe({
       next: (adminUser) => {
         this.admins.push(adminUser); // direkt zur Liste hinzufügen
-        this.closeAddModal();
+        this.closeAddModel();
         // Reset Form
         this.firstName = '';
         this.lastName = '';
